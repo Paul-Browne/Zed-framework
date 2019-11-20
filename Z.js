a23c4c8c9fd4b2f8179edeb129dde119eced99a6
paul browne <paulmatthewbrowne@gmail.com>
2019-11-20 12:10:12 +0200

    adds raw method

--- Z.js
+++ Z.js
@@ -11,6 +11,21 @@
             var x = Object.keys(obj);
             return x[x.length-1];
         }
+
+        function injector(obj, requests, resolved){
+            if (resolved.data && resolved.html) {
+                Z[x] = requests.data ? JSON.parse(resolved.data) : resolved.data;
+                obj.inner ? obj.inner.innerHTML = resolved.html : obj.outer.outerHTML = resolved.html;
+                // inject nested scripts into the head.
+                var scripts = new DOMParser().parseFromString(resolved.html, 'text/html').querySelectorAll("SCRIPT");
+                for(var i = 0; i < scripts.length; i++){
+                    var newScript = d.createElement("SCRIPT");
+                    scripts[i].src ? newScript.src = scripts[i].src : newScript.innerHTML = scripts[i].innerHTML;
+                    d.head.appendChild(newScript);
+                }
+            }
+        }
+
         // update method (simple PubSub publish)
         Z.update = function(obj) {
             topics[filt(obj)] && topics[filt(obj)](obj);
@@ -27,42 +42,33 @@
                 };
             }
             // async ajax
-            var objResolved = {};
-            var arr = obj.html ? [obj.html] : [];
-            // if data is an endpoint, push to array for xhr request, else assign it to global Z.
-            typeof obj[x] === 'string' ? arr.push(obj[x]) : Z[x] = obj[x];
-            arr.forEach(function(url, index) {
-                objResolved[index] = "";
-                var xhr = new XMLHttpRequest();
-                xhr.onreadystatechange = function() {
-                    if (xhr.readyState == 4 && xhr.status == 200) {
-                        objResolved[index] = xhr.responseText;
-                        // check if "other" xhr request has resolved
-                        var allResolved = true;
-                        for (var key in objResolved) {
-                            if (objResolved[key] == "") {
-                                allResolved = false;
-                            }
+            var resolved = {};
+            var requests = {};
+
+            typeof obj[x] === 'string' ? requests.data = obj[x] : resolved.data = obj[x];
+            
+            if(obj.raw){
+                resolved.html = obj.raw;
+            }
+            if(obj.html){
+                requests.html = obj.html;
+            }
+
+            if(resolved.html && resolved.data){
+                injector(obj, requests, resolved);
+            }else{
+                for (var req in requests){
+                    var xhr = new XMLHttpRequest();
+                    xhr.onreadystatechange = function() {
+                        if (xhr.readyState == 4 && xhr.status == 200) {
+                            resolved[req] = xhr.responseText;
+                            injector(obj, requests, resolved);
                         }
-                        if (allResolved) {
-                            if (objResolved[1]) {
-                                // assign the global before the html is injected
-                                Z[x] = JSON.parse(objResolved[1]);
-                            }
-                            obj.inner ? obj.inner.innerHTML = objResolved[0] : obj.outer.outerHTML = objResolved[0];
-                            // inject nested scripts into the head.
-                            var scripts = new DOMParser().parseFromString(objResolved[0], 'text/html').querySelectorAll("SCRIPT");
-                            for(var i = 0; i < scripts.length; i++){
-                                var newScript = d.createElement("SCRIPT");
-                                scripts[i].src ? newScript.src = scripts[i].src : newScript.innerHTML = scripts[i].innerHTML;
-                                d.head.appendChild(newScript);
-                            }
-                        }
-                    }
-                };
-                xhr.open("GET", url, true);
-                xhr.send();
-            });
+                    };
+                    xhr.open("GET", url, true);
+                    xhr.send();
+                }
+            }
         }
     }
     // init    
