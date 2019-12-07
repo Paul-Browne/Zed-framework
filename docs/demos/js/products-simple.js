35b17cf56ff8fb7826a126f21838a35b1614fe48
paul browne <paulmatthewbrowne@gmail.com>
2019-12-08 01:46:45 +0200

    removes unused files from the build

--- docs/demos/js/products-simple.js
+++
@@ -1,1 +0,0 @@
-"use strict";document.getElementById("output").innerHTML=Z.products.map(function(n,c){return'\n    <div class="product" onclick=\'Z.update("view", {data:Z.products['.concat(c,"]})'>\n        <h2>").concat(n.name,'</h2>\n        <img src="images/').concat(n.image,'" width="200">\n        <span>').concat(n.price," €</span>\n    </div>\n    ")}).join("");
