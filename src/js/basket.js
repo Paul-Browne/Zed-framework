9ca589bb8211f84db0e6c1a06a39069897b7d402
paul browne <paulmatthewbrowne@gmail.com>
2021-07-01 01:29:15 +0300

    adds shop

--- src/js/basket.js
+++ src/js/basket.js
@@ -1,64 +1,54 @@
-console.time && console.time("BASKET MOUNTED");
-Z.mount({
+Z({
   id: "basket",
-  render: function(state) {
-    var total = state.products.reduce(function(acc, current) {
+  render: function() {
+    var total = this.cart.reduce(function(acc, current) {
       return acc + current.quantity * current.price;
     }, 0);
-    return (
-      state.products
+    document.getElementById(this.id).innerHTML =
+      this.cart
         .map(function(product, index) {
           return `
           <div>
             <span>${
               product.name
-            }</span> <span><button onclick='console.time && console.time("BASKET UPDATED");Z.basket.adjustCart(${index}, -1);console.time && console.timeEnd("BASKET UPDATED");'>-</button> <span>${product.quantity}</span> <button onclick='console.time && console.time("BASKET UPDATED");Z.basket.adjustCart(${index}, 1);console.time && console.timeEnd("BASKET UPDATED");'>+</button></span> <span>${(product.price * product.quantity).toFixed(2)}</span>
+            }</span> <span><button onclick='Z.basket.adjustCart(${index}, -1);'>-</button> <span>${product.quantity}</span> <button onclick='Z.basket.adjustCart(${index}, 1);'>+</button></span> <span>${(product.price * product.quantity).toFixed(2)}</span>
           </div>`;
         })
-        .join("") + `<div><span>Total: ${total.toFixed(2)}</span></div>`
-    );
+        .join("") + `<div><span>Total: ${total.toFixed(2)}</span></div>`;
   },
-  inner: document.getElementById("basket"),
-  state: {
-    products: JSON.parse(localStorage.getItem("basket")) || [],
-    update: function() {
-      localStorage.setItem("basket", JSON.stringify(this.products));
-      Z.update("basket", {
-        state: this
-      });
-      if (Z.view.name) {
-        console.time && console.time("BUTTON UPDATED");
-        Z.update("button");
-        console.time && console.timeEnd("BUTTON UPDATED");
-      }
-    },
-    adjustCart: function(index, increment) {
-      Z.basket.products[index].quantity += increment;
-      if (!Z.basket.products[index].quantity) {
-        Z.basket.products.splice(index, 1);
-      }
-      Z.basket.update();
-    },
-
-    addToBasket: function(item) {
-      var basketObj = {
-        name: item.name,
-        price: item.price
-      };
-
-      var cartContainsItem = Z.basket.products.some(function(product, index) {
-        if (product.name === basketObj.name) {
-          product.quantity = product.quantity + 1;
-          return true;
-        }
-      });
-
-      if (!cartContainsItem) {
-        basketObj.quantity = 1;
-        Z.basket.products.push(basketObj);
-      }
-      Z.basket.update();
+  cart: JSON.parse(localStorage.getItem("basket")) || [],
+  update: function() {
+    localStorage.setItem("basket", JSON.stringify(this.cart));
+    this.render();
+    if (Z.view.state) {
+      Z.button.render();
     }
+  },
+  adjustCart: function(index, increment) {
+    this.cart[index].quantity += increment;
+    if (!this.cart[index].quantity) {
+      this.cart.splice(index, 1);
+    }
+    this.update();
+  },
+  addToBasket: function(item) {
+    var basketObj = {
+      name: item.name,
+      price: item.price
+    };
+
+    var cartContainsItem = this.cart.some(function(product, index) {
+      if (product.name === basketObj.name) {
+        product.quantity = product.quantity + 1;
+        return true;
+      }
+    });
+
+    if (!cartContainsItem) {
+      basketObj.quantity = 1;
+      this.cart.push(basketObj);
+    }
+    this.update();
   }
 });
-console.time && console.timeEnd("BASKET MOUNTED");
+Z.basket.render();
--- src/js/products-and-view.js
+++ src/js/products-and-view.js
@@ -1,22 +1,14 @@
-console.time && console.time("PRODUCTS MOUNTED");
-Z.load(
-  [
-    {
+Z.xhr({
+  id: "products",
+  url: "/json/products.json",
+  callback: function(data) {
+    Z({
       id: "products",
-      url: "/json/products.json",
-      callback: function(data) {
-        return JSON.parse(data);
-      }
-    }
-  ],
-  function() {
-    Z.mount({
-      id: "products",
-      render: function(state) {
-        return state
+      render: function() {
+        document.getElementById(this.id).innerHTML = this.data
           .map(function(product, index) {
             return `
-              <div class="product" onclick='console.time && console.time("VIEW UPDATED");Z.update("view", {state:Z.products[${index}]});console.time && console.timeEnd("VIEW UPDATED");'>
+              <div class="product" onclick='Z.view.update(${index});'>
                 <h2>${product.name}</h2>
                 <img src="/images/${product.images[0]}" width="200">
                 <span>${product.price} €</span>
@@ -24,74 +16,72 @@ Z.load(
           })
           .join("");
       },
-      inner: document.getElementById("products"),
-      state: Z.load.products
+      data: JSON.parse(data)
     });
-    console.time && console.timeEnd("PRODUCTS MOUNTED");
+    Z.products.render();
   }
-);
-console.time && console.time("VIEW MOUNTED");
-Z.mount({
+});
+
+Z({
   id: "view",
-  render: function(state) {
-    var btn = `<button onclick='console.time && console.time("BASKET UPDATED");Z.basket.addToBasket(Z.view);console.time && console.timeEnd("BASKET UPDATED");'>Add To Cart</button>`;
-    Z.basket.products.forEach(function(product, index) {
-      if (product.name === state.name) {
-        btn = `<button onclick='console.time && console.time("BASKET UPDATED");Z.basket.adjustCart(${index}, -1);console.time && console.timeEnd("BASKET UPDATED");'>-</button> <span>${product.quantity}</span> <button onclick='console.time && console.time("BASKET UPDATED");Z.basket.adjustCart(${index}, 1);console.time && console.timeEnd("BASKET UPDATED");'>+</button>`;
-      }
-    });
-    return state.name
-      ? `
-        <div class="view-modal">
-          <div class="view">
-              <button onclick='console.time && console.time("VIEW UPDATED");Z.update("view", {state:{}});console.time && console.timeEnd("VIEW UPDATED");'>close</button>
-              <h3>${state.fullname}</h3>
-              <div>
-                ${state.images
-                  .map(function(image) {
-                    return `<img src="/images/${image}" height="150">`;
-                  })
-                  .join("")}
-              </div>
-              <p>${state.description}</p>
-              <div class="included">
-              ${state.included
-                .map(function(inc) {
-                  return `
-                    <div>
-                      <img src="/images/details/${inc.image}" height="40">
-                      <p>${inc.name}</p>
-                    </div>`;
-                })
-                .join("")}
-              </div>
-              <p>${state.price} €</p>
-              <div id="button"></div>
-          </div>
-        </div>
-        `
-      : "";
-  },
-  after: function(viewState) {
-    if (viewState.name) {
-      console.time && console.time("BUTTON MOUNTED");
-      Z.mount({
-        id: "button",
-        render: function() {
-          var btn = `<button onclick='console.time && console.time("BASKET UPDATED");Z.basket.addToBasket(Z.view);console.time && console.timeEnd("BASKET UPDATED");'>Add To Cart</button>`;
-          Z.basket.products.forEach(function(product, index) {
-            if (product.name === viewState.name) {
-              btn = `<button onclick='console.time && console.time("BASKET UPDATED");Z.basket.adjustCart(${index}, -1);console.time && console.timeEnd("BASKET UPDATED");'>-</button> <span>${product.quantity}</span> <button onclick='console.time && console.time("BASKET UPDATED");Z.basket.adjustCart(${index}, 1);console.time && console.timeEnd("BASKET UPDATED");'>+</button>`;
-            }
-          });
-          return btn;
-        },
-        inner: document.getElementById("button")
+  render: function() {
+    if (this.state) {
+      var btn = `<button onclick='Z.basket.addToBasket(Z.view.state);'>Add To Cart</button>`;
+      Z.basket.cart.forEach((product, index) => {
+        if (product.name === this.state.name) {
+          btn = `<button onclick='Z.basket.adjustCart(${index}, -1);'>-</button> <span>${product.quantity}</span> <button onclick='Z.basket.adjustCart(${index}, 1);'>+</button>`;
+        }
       });
-      console.time && console.timeEnd("BUTTON MOUNTED");
+      document.getElementById("view").innerHTML = `
+      <div class="view-modal">
+        <div class="view">
+          <button onclick='Z.view.update();'>close</button>
+          <h3>${this.state.fullname}</h3>
+          <div>
+            ${this.state.images
+              .map(function(image) {
+                return `<img src="/images/${image}" height="150">`;
+              })
+              .join("")}
+          </div>
+          <p>${this.state.description}</p>
+          <div class="included">
+          ${this.state.included
+            .map(function(inc) {
+              return `
+                <div>
+                  <img src="/images/details/${inc.image}" height="40">
+                  <p>${inc.name}</p>
+                </div>`;
+            })
+            .join("")}
+          </div>
+          <p>${this.state.price} €</p>
+          <div id="button"></div>
+        </div>
+      </div>
+      `;
+    } else {
+      document.getElementById("view").innerHTML = "";
     }
   },
-  inner: document.getElementById("view"),
-  state: {}
+  update: function(index) {
+    this.state = index >= 0 ? Z.products.data[index] : null;
+    this.render();
+    this.state && Z.button.render();
+  },
+  state: null
 });
-console.time && console.timeEnd("VIEW MOUNTED");
+
+Z({
+  id: "button",
+  render: function() {
+    var btn = `<button onclick='Z.basket.addToBasket(Z.view.state);'>Add To Cart</button>`;
+    Z.basket.cart.forEach((product, index) => {
+      if (product.name === Z.view.state.name) {
+        btn = `<button onclick='Z.basket.adjustCart(${index}, -1);'>-</button> <span>${product.quantity}</span> <button onclick='Z.basket.adjustCart(${index}, 1);'>+</button>`;
+      }
+    });
+    document.getElementById(this.id).innerHTML = btn;
+  }
+});
